937a047761bbd16a2a10216e24f4db96f610ed18
AarogyaPandey <aarogya.pandey5@gmail.com>
2024-03-22 17:07:37 +0545

    feat/added-filter-and-pagination

--- todo/models.py
+++ todo/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 from django.contrib.auth.models import User
-# Create your models here.
 
 class Task(models.Model):
     '''
@@ -13,8 +12,32 @@ class Task(models.Model):
     user=models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
     title=models.CharField(max_length=100)
     description=models.TextField(null=True, blank=True)
-    complete=models.BooleanField(default=False)
-    created=models.DateTimeField(auto_now_add=True)
+    is_completed=models.BooleanField(default=False)
+    is_created=models.DateTimeField(auto_now_add=True)
+    due_date=models.DateTimeField(blank=True, null=True) 
+    estimated_hours=models.FloatField(default=0) #this will be used for time tracking 
+    
+    
+    class Category(models.TextChoices):
+        """
+        This  class contains different categories of tasks that can be assigned.
+        """
+        WORK= "WK", "Work"
+        PERSONAL= "PRSNL", "Personal"
+        STUDY= "STDY", "Study"
+        HEALTH= "HLT", "Health"
+        FINANCE= "FNS", "Finance"
+        HOME= "HM" , "Home"
+        SOCIAL= "SCL", "Social"
+        SHOPPING= "SPNG","Shopping"
+        ENTERTAINMENT= "ENTNT" ,"Entertainment"
+        
+    category=models.CharField( 
+        max_length=5,
+        choices=Category.choices,
+        default=Category.WORK,
+    )
+    
     
     def __str__(self):
         '''
@@ -22,8 +45,9 @@ class Task(models.Model):
         '''
         return self.title
     
+    
     class Meta:
         '''
         This class contains additional info of model (Task).
         '''
-        ordering=['complete']
+        ordering=['is_completed']
