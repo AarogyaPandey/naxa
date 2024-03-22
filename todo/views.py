01a2383624cfbb17ee93f7f25435891e6d9e491c
AarogyaPandey <aarogya.pandey5@gmail.com>
2024-03-22 09:33:14 +0545

    feat/add-pagination-in-listapi

--- todo/views.py
+++ todo/views.py
@@ -4,28 +4,38 @@ from rest_framework import viewsets, status
 from django.http import JsonResponse
 from .models import Task
 from rest_framework.response import Response
-
+from rest_framework.pagination import PageNumberPagination
+# from rest_framework.pagination import CustomPageNumberPagination
+class MyPagination(PageNumberPagination):
+    # pagination_class= PageNumberPagination
+    page_size=2
+    page_size_query_param='page_size'
+    max_page_size=2
+        
 class TodoViewSet(viewsets.ModelViewSet):
     '''
     this class  defines the create, read, update and delete functionality.
     '''
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
-   
+    pagination_class=MyPagination
     
     def list(self, request, *args, **kwargs):
         '''
         this func deals with the filter if the  user wants to get all tasks or only completed ones.
         '''
         completed = self.request.query_params.get("completed", None)
-        print('hello', completed)
+        # print('hello', completed)
         # completed = self.request.data.get( "completed", "true" )
         if completed == "yes":
-            queryset = Task.objects.filter(complete = True)
+            self.queryset = Task.objects.filter(complete = True)
         else:
-            queryset = Task.objects.all()
-        serializer = self.serializer_class(queryset, many=True)
-        return Response({"result":serializer.data})
+            self.queryset = Task.objects.all()
+            
+        return super().list(request,*args,**kwargs)
+        # serializer = self.serializer_class(queryset, many=True) 
+        # return Response({"result":serializer.data})
+        # return super().list(request,*args,**kwargs)
     
     def destroy(self, request, *args, **kwargs):
         '''
