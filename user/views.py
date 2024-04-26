4ee7acecb1e668cb9fd63e623d000e7548c88fef
AarogyaPandey <aarogya.pandey5@gmail.com>
2024-04-26 16:48:36 +0545

    fix/login-and-auth

--- user/views.py
+++ user/views.py
@@ -18,12 +18,15 @@ class SignUp(APIView):
 
 @api_view(["POST"])
 def postapi(request):
+    print(request.data)
     user = authenticate(username =request.data["username"], password = request.data["password"] )
     print(user)
     if user:
-        token = Token.objects.get_or_create(user=user)
-        return Response({"token": token.key,
-                         "user_id":user.id,})
+        token, _ = Token.objects.get_or_create(user=user)
+        return Response({
+            "token": token.key,
+            "user_id":user.id,
+            })
     else:
         return Response({"error": "Invalid credentials"}, status=401)
 
